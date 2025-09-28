f01646845e6c174d36231fc13c896ede1484e996
RianHidayatSaputra <jendraltengue123@gmail.com>
2025-09-28 13:51:50 +0700

    add rules import

--- app/Imports/GuruImport.php
+++ app/Imports/GuruImport.php
@@ -5,9 +5,9 @@ namespace App\Imports;
 use App\Models\Guru;
 use Illuminate\Support\Collection;
 use Maatwebsite\Excel\Concerns\ToModel;
-use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithStartRow;
 
-class GuruImport implements ToModel, WithHeadingRow
+class GuruImport implements ToModel, WithStartRow
 {
     /**
     * @param Collection $collection
@@ -15,14 +15,19 @@ class GuruImport implements ToModel, WithHeadingRow
     public function model(array $row)
     {
         return new Guru([
-            'nama' => $row['nama'],
-            'nuptk' => (string) $row['nuptk'],
-            'jabatan' => $row['jabatan'],
-            'status_kepegawaian' => $row['status_kepegawaian'],
-            'alamat' => $row['alamat'],
-            'kontrak_guru' => $row['kontrak_guru'],
-            'no_kartu' => (string) $row['no_kartu'],
-            'no_wa' => (string) $row['no_wa'],
+            'nama' => $row[0],
+            'nuptk' => (string) $row[1],
+            'jabatan' => $row[2],
+            'status_kepegawaian' => $row[3],
+            'alamat' => $row[4],
+            'kontrak_guru' => $row[5],
+            'no_kartu' => (string) $row[6],
+            'no_wa' => (string) $row[7],
         ]);
+    }
+
+    public function startRow(): int
+    {
+        return 9;
     }
 }
